1f95862c1dda7d644072a89255235e8b4f99bfef
Shuan Wang <shuanwang@gmail.com>
2012-02-03 03:35:38 -0800

    adding 2nd problem

---
+++ euler002.clj
@@ -0,0 +1,24 @@
+(comment
+	; took me a while, trying to figure out how to generate a list based on what i wanted
+	; eventually found an example that generated a lazy-list using iterate
+	; also figured out passing in closures (lambdas?) to represent a predicate in take-while 
+	; and instead of using (fn) use #() shorthand.
+)
+(print
+
+	(reduce 
+		+ (filter even? 
+			(take-while 
+				#(< % 4000000)
+				(map 
+					first 
+					(iterate 
+						(fn [[a,b]] [b (+ a b)]) 
+						[1 1]
+					)
+				)
+			)
+		)
+	)
+
+)
