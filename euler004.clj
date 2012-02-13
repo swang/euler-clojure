23bcefaaa7b68a4a63ff61e4c89d0594d2822aae
Shuan Wang <shuanwang@gmail.com>
2012-02-12 22:43:29 -0800

    fixed is-palidrome using num to str list conversions from Euler #8

--- euler004.clj
+++ euler004.clj
@@ -2,36 +2,39 @@
 ;Find the largest palindrome made from the product of two 3-digit numbers.
 (use '[clojure.string :only (split)])
 
-(def num-to-str-coll (fn [x]
-	"converts a number into a collection of digits"
-	(rest (split (Integer/toString x) #"") )
+(def num-to-str-list (fn [x]
+	^{ 
+	    :doc "converts a number x into a list of digits"
+	    :test (fn []    (is (= (num-to-str-list 1234) ["1" "2" "3" "4"])))
+	}
+	(into [] (rest (split (Integer/toString x) #"") ))
 ))
-(def coll-to-num (fn [x]
-	"converts a collection like ("1" "2" "3" "4" into a number: 1234 "
-	;(if (> (count x) 0 )
-		(Integer/parseInt (apply str x))
-	;	0
-	;)
-))
+
 (def is-palidrome (fn [x]
-	(if (< (count (num-to-str-coll x)) 2)
-		true
-		(if (= (first (num-to-str-coll x)) (last (num-to-str-coll x)))
-			(is-palidrome (coll-to-num (butlast (rest (num-to-str-coll x)))))
-			false
-		)
-	)
+    ^{
+        :doc "returns true if number x reads the same front and back"
+        :test (fn []    (is (= (is-palidrome 1234) false))
+                        (is (= (is-palidrome 1221) true))
+                        (is (= (is-palidrome 1) true))
+                        (is (= (is-palidrome 12213) false))
+        )
+    }
+    (loop [number (num-to-str-list x)]
+        (if (or (empty? number) (= 1 (count number)))
+            true
+            (if (not (= (first number) (last number)))
+                false
+                (recur (into [] (rest (butlast number))))
+            )
+        )
+    )
 ))
-
-
-(println (is-palidrome 1234))
-
-(println (is-palidrome 4444))
-(println (reduce max (for [
-	a (range 100 1000)
-	b (range 100 1000)
-	;:when (< a 15)
- 	:when ( is-palidrome (* a b))
-	]
-	(* a b)
-)))
+(println 
+    (reduce max (for [
+        a (range 100 1000)
+        b (range 100 1000)
+        	:when ( is-palidrome (* a b))
+        ]
+        (* a b)
+    ))
+)
