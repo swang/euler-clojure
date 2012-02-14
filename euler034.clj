9f10e9de489cec6b666e06c310ee4ffbe8d20977
Shuan Wang <shuanwang@gmail.com>
2012-02-14 01:47:06 -0800

    minor cosmetic changes to comment section on top

--- euler034.clj
+++ euler034.clj
@@ -7,8 +7,10 @@
 ; Find the sum of all numbers which are equal to the sum of the factorial of their digits.
 ;
 ; Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;
 ; 40730
+
 (use '[clojure.string :only (split)])
 
 (def fact (fn [num]
