655171bbb97695564a3f2092bd859267b11a8b77
Shuan Wang <shuanwang@gmail.com>
2012-02-14 01:57:04 -0800

    Adding minor cosmetic change to comments in Project Euler #30

--- euler030.clj
+++ euler030.clj
@@ -12,6 +12,7 @@
 ; The sum of these numbers is 1634 + 8208 + 9474 = 19316.
 ;
 ; Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;
 ; 443839
 
