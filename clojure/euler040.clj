bf77083ec886209e00215dad2645b43f365ac2da
Shuan Wang <shuanwang@gmail.com>
2012-02-22 21:28:32 -0800

    Added Project Euler #40

---
+++ clojure/euler040.clj
@@ -0,0 +1,47 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Euler #40
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; An irrational decimal fraction is created by concatenating the positive integers:
+; 
+; 0.123456789101112131415161718192021...
+;              ^
+;              +----------....
+; It can be seen that the 12th digit of the fractional part is 1.
+; 
+; If d(n) represents the nth digit of the fractional part, find the value of the following expression.
+; 
+; d(1) x d(10) x d(100) x d(1000) x d(10000) x d(100000) x d(1000000)
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; 210
+;
+
+(use '[clojure.string :only (split)])
+
+(defn gen-seq [n]
+    (cons 0 (map #(Integer/parseInt %) 
+         (rest 
+            (split 
+                (apply str (range 1 (inc n)))
+            #"")
+        )
+    ))
+)
+
+ 
+(def frac-part 
+    (gen-seq (Math/ceil (/ (+ 1000000 9) 2)) )
+) 
+(println 
+    (*
+        (nth frac-part 1)
+        (nth frac-part 10)
+        (nth frac-part 100)
+        (nth frac-part 1000)
+        (nth frac-part 10000)
+        (nth frac-part 100000)
+        (nth frac-part 1000000)
+    )
+)
