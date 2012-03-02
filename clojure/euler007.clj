a3d4800ddfc209dc590dab3d313ca2e67a88a9e3
Shuan Wang <shuanwang@gmail.com>
2012-03-01 22:52:49 -0800

    Added Project Euler #7

---
+++ clojure/euler007.clj
@@ -0,0 +1,18 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Euler #7
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see 
+; that the 6th prime is 13.
+;
+; What is the 10,001st prime number?
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; 104743
+;
+
+(use '[clojure.contrib.lazy-seqs :only (primes)])
+
+(println (nth primes (- 10001 1))) ;nth is 0-indexed
+
